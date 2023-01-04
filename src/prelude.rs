f4002e58ef01b0f01f4c6d9d8acd2fa511985a74
Corey Shupe <coreyshupe101@gmail.com>
2023-01-04 16:04:07 -0500

    Be specific.

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,13 @@
 pub use crate::transport::{
     buffer::{DraxReadExt, DraxWriteExt},
     error::{ErrorType, TransportError, TransportErrorContext},
-    packet::{option::*, primitive::*, serde_json::JsonDelegate, vec::*, PacketComponent, Size},
+    packet::{
+        option::Maybe,
+        primitive::{VarInt, VarLong},
+        serde_json::JsonDelegate,
+        vec::{ByteDrain, SliceU8, VecU8},
+        PacketComponent, Size,
+    },
     Result,
 };
 pub use tokio::io::{AsyncRead, AsyncWrite};
