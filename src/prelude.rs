e145246b84f27c18c16e68f2900e721e35638b81
Corey Shupe <coreyshupe101@gmail.com>
2023-01-04 16:07:03 -0500

    Hide delegates.

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,13 +1,6 @@
 pub use crate::transport::{
     buffer::{DraxReadExt, DraxWriteExt},
     error::{ErrorType, TransportError, TransportErrorContext},
-    packet::{
-        option::Maybe,
-        primitive::{VarInt, VarLong},
-        serde_json::JsonDelegate,
-        vec::{ByteDrain, SliceU8, VecU8},
-        PacketComponent, Size,
-    },
     Result,
 };
 pub use tokio::io::{AsyncRead, AsyncWrite};
