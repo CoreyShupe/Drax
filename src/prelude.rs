278cc57ec329d8283e83ffc1825dda47fd0dc14a
Corey Shupe <coreyshupe101@gmail.com>
2023-01-04 16:07:29 -0500

    Export packet component

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,7 @@
 pub use crate::transport::{
     buffer::{DraxReadExt, DraxWriteExt},
     error::{ErrorType, TransportError, TransportErrorContext},
+    packet::PacketComponent,
     Result,
 };
 pub use tokio::io::{AsyncRead, AsyncWrite};
