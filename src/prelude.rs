5f2399a39d8aa5a5029c529bb2ee7b0254a402b7
Corey Shupe <coreyshupe101@gmail.com>
2023-01-04 16:07:51 -0500

    Export Size

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,7 @@
 pub use crate::transport::{
     buffer::{DraxReadExt, DraxWriteExt},
     error::{ErrorType, TransportError, TransportErrorContext},
-    packet::PacketComponent,
+    packet::{PacketComponent, Size},
     Result,
 };
 pub use tokio::io::{AsyncRead, AsyncWrite};
