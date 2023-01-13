0aae9db53ba42957c70952dd6689b7ad42c4bb7c
Corey Shupe <coreyshupe101@gmail.com>
2023-01-12 21:13:17 -0500

    Add helper component macros

--- src/transport/packet.rs
+++ src/transport/packet.rs
@@ -111,6 +111,124 @@ pub mod vec;
 
 #[cfg(feature = "macros")]
 pub mod macros {
+    macro_rules! component_internal {
+        ($(#[$($tt:tt)*])* enum $enum_name:ident {
+            $key_name:ident: $key_delegate_type:ty,
+            $(@ser_delegate $static_product_delegate_type:ty,)?
+            $(@match $key_matcher:expr,)?
+            $(
+                $(#[$($vtt:tt)*])*
+                $($key_matcher_case:literal =>)? $variant_name:ident {
+                    $(
+                        $(
+                            $(#[$($ftt:tt)*])*
+                            $field_name:ident: $delegate_type:ty,
+                        )+
+                    )?
+                }
+            ),*
+        }) => {
+            $crate::enum_packet_components! {
+                $(#[$($tt)*])*
+                $enum_name {
+                    $key_name: $key_delegate_type,
+                    $(@ser_delegate $static_product_delegate_type,)?
+                    $(@match $key_matcher,)?
+                    $(
+                        $(#[$($vtt)*])*
+                        $($key_matcher_case =>)? $variant_name {
+                            $(
+                                $(
+                                    $(#[$($ftt)*])*
+                                    $field_name: $delegate_type
+                                ),+
+                            )?
+                        }
+                    ),*
+                }
+            }
+        };
+        ($(#[$($tt:tt)*])* struct $struct_name:ident {
+            $(
+                $(
+                    $(#[$($doc_tt:tt)*])*
+                    $field_name:ident: $delegate_type:ty,
+                )+
+            )?
+        }) => {
+            $crate::struct_packet_components! {
+                $(#[$($tt)*])*
+                $struct_name {
+                    $(
+                        $(
+                            $(#[$($doc_tt)*])*
+                            $field_name: $delegate_type
+                        ),+
+                    )?
+                }
+            }
+        };
+    }
+
+    #[macro_export]
+    macro_rules! components {
+        ($(
+            $(#[$($tt2:tt)*])* // any extra attributes for the struct
+            $(enum $component_enum_name:ident<$(C: $c_e_ctx_ty:ty,)? $c_key_name:ident: $c_key_delegate_type:ty> {
+                $(@ser_delegate $c_static_product_delegate_type:ty,)?
+                $(@match $c_key_matcher:expr,)?
+                $( // enum field delegations
+                    $(#[$($cvtt:tt)*])*
+                    $c_variant_name:ident {
+                        $(@key($c_key_matcher_case:literal);)?
+                        $(
+                            $(
+                                $(#[$($cftt:tt)*])*
+                                $c_v_field_name:ident: $c_v_delegate_type:ty
+                            ),+
+                        )?
+                    }
+                ),*
+            })?
+            $(struct $component_struct_name:ident $(<$c_ctx_ty:ty>)? {
+                $(
+                    $(
+                        $(#[$($c_doc_tt:tt)*])*
+                        $c_field_name:ident: $c_delegate_type:ty
+                    ),+
+                )?
+            })?
+        ),*) => {
+            $(registry_internal! {
+                $(#[$($tt2)*])*
+                $(enum $component_enum_name$(<$c_e_ctx_ty>)? {
+                    $c_key_name: $c_key_delegate_type,
+                    $(@ser_delegate $c_static_product_delegate_type,)?
+                    $(@match $c_key_matcher,)?
+                    $( // enum field delegations
+                        $(#[$($cvtt)*])*
+                        $($c_key_matcher_case =>)? $c_variant_name {
+                            $(
+                                $(
+                                    $(#[$($cftt)*])*
+                                    $c_v_field_name: $c_v_delegate_type,
+                                )+
+                            )?
+                        }
+                    ),*
+                })?
+                $(struct $component_struct_name$(<$c_ctx_ty>)? {
+                $( // struct field delegations
+                    $(
+                        $(#[$($c_doc_tt)*])*
+                        $c_field_name: $c_delegate_type,
+                    )+
+                )?
+                })?
+            })*
+        };
+    }
+
     #[macro_export]
     macro_rules! enum_packet_components {
         (@internal @match $key_ident:ident) => {
