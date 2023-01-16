db8a2f83d97ca3ef7f7ff6faf4443705f0b63599
Corey Shupe <coreyshupe101@gmail.com>
2023-01-16 12:57:55 -0500

    Decrypt temp.

--- src/transport/buffer.rs
+++ src/transport/buffer.rs
@@ -1,141 +1,9 @@
+use tokio::io::{AsyncRead, AsyncWrite};
+
 use crate::prelude::PacketComponent;
 use crate::transport::buffer::var_num::{ReadVarInt, ReadVarLong, WriteVarInt, WriteVarLong};
-use crate::{err_explain, PinnedLivelyResult};
-use std::pin::Pin;
-use std::task::{ready, Context, Poll};
-use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
-
-/// A trait extension for `AsyncRead` which limits a stream.
-pub trait Limiter {
-    /// Limits the stream to a certain number of bytes. Error on overflow.
-    fn hard_limit(&mut self, limit: usize) -> ReadLimiter<'_, Self>
-    where
-        Self: Sized;
-}
-
-impl<T> Limiter for T
-where
-    T: AsyncRead + Unpin + Send + Sync,
-{
-    fn hard_limit(&mut self, limit: usize) -> ReadLimiter<'_, Self>
-    where
-        Self: Sized,
-    {
-        ReadLimiter::new(self, limit)
-    }
-}
-
-/// A reader wrapper that limits the number of bytes that can be read from the underlying reader.
-///
-/// The `ReadLimiter` struct wraps an `AsyncRead` object and provides a method for limiting the number of bytes
-/// that can be read from the underlying reader. When the limit is reached, any further read operations will return
-/// an error with the message "Read limit exceeded".
-pub struct ReadLimiter<'a, A> {
-    reader: &'a mut A,
-    limit: usize,
-    current: usize,
-}
-
-impl<'a, A> ReadLimiter<'a, A> {
-    /// Creates a new `ReadLimiter` that wraps the given reader and limits the number of bytes that can be read
-    /// from the reader to the given number.
-    ///
-    /// # Parameters
-    ///
-    /// - `reader`: The reader to wrap.
-    /// - `limit`: The maximum number of bytes that can be read from the reader.
-    ///
-    /// # Returns
-    ///
-    /// A new `ReadLimiter` that wraps the given reader and limits the number of bytes that can be read from the
-    /// reader to the given number.
-    ///
-    /// # Examples
-    ///
-    /// A `ReadLimiter` will throw an error if a limit is reached, even prior to read:
-    /// ```
-    /// # use std::io::Cursor;
-    /// # use tokio_test::{assert_err, assert_ok};
-    /// # use drax::transport::buffer::ReadLimiter;
-    /// # use tokio::io::AsyncReadExt;
-    /// let mut cursor = Cursor::new(vec![1u8, 2, 3]);
-    /// let mut limiter = ReadLimiter::new(&mut cursor, 2);
-    /// let mut buf = [0; 3];
-    /// assert_err!(tokio_test::block_on(async { limiter.read_exact(&mut buf).await }));
-    /// ```
-    ///
-    /// If a read is exactly at the limit, no error will be thrown and it will pass through as expected.
-    /// ```
-    /// # use std::io::Cursor;
-    /// # use tokio_test::{assert_err, assert_ok};
-    /// # use drax::transport::buffer::ReadLimiter;
-    /// # use tokio::io::AsyncReadExt;
-    /// let mut cursor = Cursor::new(vec![1u8, 2, 3]);
-    /// let mut limiter = ReadLimiter::new(&mut cursor, 2);
-    /// let mut buf = [0; 2];
-    /// assert_ok!(tokio_test::block_on(async { limiter.read_exact(&mut buf).await }));
-    /// assert_eq!(buf, [1, 2]);
-    /// ```
-    pub fn new(reader: &'a mut A, limit: usize) -> Self {
-        Self {
-            reader,
-            limit,
-            current: 0,
-        }
-    }
-
-    /// Checks that the entire specified number of bytes has been read from the reader.
-    /// If the number of bytes read is less than the specified number, an error is returned.
-    ///
-    /// # Examples
-    /// ```
-    /// # use std::io::Cursor;
-    /// # use tokio_test::{assert_err, assert_ok};
-    /// # use drax::transport::buffer::ReadLimiter;
-    /// # use tokio::io::AsyncReadExt;
-    /// let mut cursor = Cursor::new(vec![1u8, 2, 3]);
-    /// let mut limiter = ReadLimiter::new(&mut cursor, 2);
-    /// let mut buf = [0; 1];
-    /// assert_ok!(tokio_test::block_on(async { limiter.read_exact(&mut buf).await }));
-    /// assert_err!(limiter.assert_length());
-    /// ```
-    pub fn assert_length(&self) -> crate::transport::Result<()> {
-        if self.current == self.limit {
-            Ok(())
-        } else {
-            Err(err_explain!(
-                "Buffer under-read, failed to read whole buffer"
-            ))
-        }
-    }
-}
-
-impl<'a, A> AsyncRead for ReadLimiter<'a, A>
-where
-    A: AsyncRead + Unpin + Send + Sync,
-{
-    fn poll_read(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<std::io::Result<()>> {
-        let filled_current = buf.filled().len();
-        if self.current + buf.remaining() > self.limit {
-            return Poll::Ready(Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Read limit exceeded",
-            )));
-        }
-
-        // if the remaining + the current is not greater than the limit then there's no way
-        // we could possible read more bytes than the limit
-        ready!(Pin::new(&mut *self.reader).poll_read(cx, buf))?;
-        let filled = buf.filled().len() - filled_current;
-        self.current += filled;
-
-        Poll::Ready(Ok(()))
-    }
-}
+use crate::transport::encryption::{Cipher, CipherAttachedReader};
+use crate::PinnedLivelyResult;
 
 pub trait DraxReadExt {
     fn read_var_int(&mut self) -> ReadVarInt<'_, Self>;
@@ -148,6 +16,10 @@ pub trait DraxReadExt {
     ) -> PinnedLivelyResult<'a, P::ComponentType>
     where
         P: Sized;
+
+    fn decrypt(&mut self, cipher: Cipher) -> CipherAttachedReader<Self>
+    where
+        Self: Sized;
 }
 
 impl<T> DraxReadExt for T
@@ -170,6 +42,16 @@ where
         P: Sized,
     {
         P::decode(context, self)
+    }
+
+    fn decrypt(&mut self, cipher: Cipher) -> CipherAttachedReader<Self>
+    where
+        Self: Sized,
+    {
+        CipherAttachedReader {
+            inner: self,
+            cipher,
+        }
     }
 }
 
@@ -207,13 +89,15 @@ where
 }
 
 pub mod var_num {
-    use crate::{err, err_explain};
-    use pin_project_lite::pin_project;
     use std::future::Future;
     use std::marker::PhantomPinned;
     use std::pin::Pin;
     use std::task::{ready, Context, Poll};
+
+    use pin_project_lite::pin_project;
     use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+
+    use crate::{err, err_explain};
 
     macro_rules! declare_var_num_ext {
         (
@@ -377,8 +261,9 @@ pub mod var_num {
 
 #[cfg(test)]
 mod tests {
-    use super::{DraxReadExt, DraxWriteExt};
     use std::io::Cursor;
+
+    use super::{DraxReadExt, DraxWriteExt};
 
     // read ext
 
--- src/transport/encryption.rs
+++ src/transport/encryption.rs
@@ -1,4 +1,30 @@
+use std::pin::Pin;
+use std::task::{ready, Context, Poll};
+
+use aes::cipher::AsyncStreamCipher;
+pub use aes::cipher::NewCipher;
+use pin_project_lite::pin_project;
+use tokio::io::{AsyncRead, ReadBuf};
+
 /// Encryption type alias for `cfb8::Encryptor<Aes128>`
 pub type Cipher = cfb8::Cfb8<aes::Aes128>;
 
-pub use aes::cipher::NewCipher;
+pin_project! {
+    pub struct CipherAttachedReader<'a, R> {
+        pub(crate) inner: &'a mut R,
+        pub(crate) cipher: Cipher,
+    }
+}
+
+impl<'a, R: AsyncRead + Unpin> AsyncRead for CipherAttachedReader<'a, R> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        let me = self.project();
+        ready!(Pin::new(me.inner).poll_read(cx, buf))?;
+        me.cipher.decrypt(buf.filled_mut());
+        Poll::Ready(Ok(()))
+    }
+}
